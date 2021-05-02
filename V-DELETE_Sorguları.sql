3539df6e609fd896a7f1122b5afe1ee374bceb3e
Berkay ERAYDIN <54270184+eraydin61@users.noreply.github.com>
2021-05-02 20:56:51 +0300

    Add files via upload

---
+++ V-DELETE_Sorguları.sql
@@ -0,0 +1,26 @@
+﻿-- DELETE Sorguları
+
+-- Ör: Tüm EMPLOYEE tablosunun silinmesi!
+-- Diger tablolarda yabancı anahtar kuralları :
+-- CASCADE --> Diger tablolardaki ilgili kayıtlar da silinir!
+-- SET NULL --> Diger tablolarda kayıtlar silinmez. Yabancı 
+-- anahtar değeri NULL yapılır.
+DELETE FROM EMPLOYEE
+
+-- Soyadı 'Brown' olan çalışanların tablodan silinmesi
+DELETE FROM EMPLOYEE
+WHERE Lname = 'Brown'
+
+-- 'Research departmanında çalışanların silinmesi
+DELETE FROM EMPLOYEE
+WHERE dno IN (SELECT dnumber
+				FROM DEPARTMENT
+				  WHERE dname = 'Research')
+
+--- '777777777' ssn numaralı kişinin silinmesi
+-- DEPARTMENT tablosunda müdür olduğu departman için ilgili alan NULL
+-- WORKS_ON ve DEPENDENT tablolarında ilgili kayıtlar silinir (CASCADE)
+DELETE FROM EMPLOYEE
+WHERE ssn = '777777777'
+
+
---
+++ VII-Farklı_JOIN_Yöntemleri.sql
@@ -0,0 +1,18 @@
+﻿-- 'Administration' departmanının yöneticisinin adı, soyadı
+
+-- 1. yöntem
+SELECT E.fname, E.lname
+FROM EMPLOYEE AS E, DEPARTMENT AS D
+WHERE E.ssn = D.mgr_ssn
+AND D.dname = 'Administration'
+
+-- 2. yöntem
+SELECT E.fname, E.lname
+FROM EMPLOYEE AS E JOIN DEPARTMENT AS D ON E.ssn = D.mgr_ssn
+WHERE D.dname = 'Administration'
+
+-- 3. yöntem
+SELECT fname, lname
+FROM EMPLOYEE
+WHERE ssn IN (SELECT mgr_ssn FROM DEPARTMENT
+			 WHERE dname = 'Administration')
